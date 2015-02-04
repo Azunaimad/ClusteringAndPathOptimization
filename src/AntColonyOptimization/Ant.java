1e743d6529b2063c2de109e38893d30529fd1b67
Azunaimad <gthtcels@gmail.com>
2015-02-04 19:01:01 +0300

    created function prototypes for Ant: -countNewTownIndex -countProbabilityDistribution -makeInitialAntRoute -makeNewAntRoute -swapTowns
    and 2 variables:
    -antRoute
    -numberOfTowns

--- src/AntColonyOptimization/Ant.java
+++ src/AntColonyOptimization/Ant.java
@@ -1,10 +1,40 @@
 package AntColonyOptimization;
 
-/**
- * Created by Azunai on 04.02.2015.
- */
 public class Ant {
+
+    protected int[] antRoute;
+    protected int numberOfTowns;
+
+
     public Ant(){
-        
+        /* TODO */
     }
+
+    protected void makeInitialAntRoute(){
+        /* TODO */
+    }
+
+    protected void makeNewAntRoute(double[][] pheromonesMatrix, double[][] visibilityMatrix){
+        /* TODO */
+    }
+
+    private int countNewTownIndex(double[] probabilityDistribution, double happenedProbability){
+        int newTownIndex = 0;
+        /* TODO */
+        return newTownIndex;
+    }
+
+    private double [] countProbabilityDistribution(double[][] pheromonesMatrix,
+                                                   double[][] visibilityMatrix,
+                                                   int currentTown,
+                                                   int[] nextTowns){
+        double[] Probability = new double[nextTowns.length];
+        /* TODO */
+        return Probability;
+    }
+
+    private void swapTowns(int oldTownIndex, int newTownIndex){
+        /* TODO */
+    }
+
 }
