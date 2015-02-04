35acf4eb48eab8a67f40221bb27d7f6058ef1a85
Azunaimad <gthtcels@gmail.com>
2015-02-04 19:03:33 +0300

    done: countNewTownIndex makeNewAntRoute swapTowns

--- src/AntColonyOptimization/Ant.java
+++ src/AntColonyOptimization/Ant.java
@@ -1,5 +1,7 @@
 package AntColonyOptimization;
 
+import java.util.Random;
+
 public class Ant {
 
     protected int[] antRoute;
@@ -10,17 +12,19 @@ public class Ant {
         /* TODO */
     }
 
-    protected void makeInitialAntRoute(){
-        /* TODO */
-    }
-
-    protected void makeNewAntRoute(double[][] pheromonesMatrix, double[][] visibilityMatrix){
-        /* TODO */
-    }
-
     private int countNewTownIndex(double[] probabilityDistribution, double happenedProbability){
         int newTownIndex = 0;
-        /* TODO */
+        int probSum = 0;
+        for(int j=0; j<probabilityDistribution.length; j++){
+            probSum+= probabilityDistribution[j];
+            if (happenedProbability<probSum){
+                // newTownIndex = i-1+j = j - (numberOfTowns - i) + numberOfTowns - 1 =
+                // = j - nextTowns.length + numberOfTowns - 1;
+                // nextTowns.length = probabilityDistribution.length
+                newTownIndex = j - probabilityDistribution.length + numberOfTowns - 1;
+                break;
+            }
+        }
         return newTownIndex;
     }
 
@@ -33,8 +37,28 @@ public class Ant {
         return Probability;
     }
 
-    private void swapTowns(int oldTownIndex, int newTownIndex){
+    protected void makeInitialAntRoute(){
         /* TODO */
+    }
+
+    protected void makeNewAntRoute(double[][] pheromonesMatrix, double[][] visibilityMatrix){
+        for(int i=1; i<numberOfTowns-1; i++){
+            int currentTown = antRoute[i-1];
+            int[] nextTowns = new int[numberOfTowns - i];
+            //Get ant next towns
+            System.arraycopy(antRoute, i, nextTowns, i, numberOfTowns - i);
+            double[] probabilityDistribution = countProbabilityDistribution(pheromonesMatrix,
+                    visibilityMatrix, currentTown, nextTowns);
+            double happenedProbability = (new Random()).nextDouble();
+            int newTownIndex = countNewTownIndex(probabilityDistribution, happenedProbability);
+            swapTowns(i, newTownIndex);
+        }
+    }
+
+    private void swapTowns(int oldTownIndex, int newTownIndex){
+        int temp = antRoute[newTownIndex];
+        antRoute[newTownIndex] = antRoute[oldTownIndex];
+        antRoute[oldTownIndex] = temp;
     }
 
 }
